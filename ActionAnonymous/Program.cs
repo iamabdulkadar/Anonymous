4e8f435b32d818f1ef7cd8e2f8899af69db74e65
abdul.kr <abdul.kr@PIA3400>
2022-12-08 09:03:02 +0530

    Add project files.

---
+++ ActionAnonymous/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ActionAnonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> addAction = delegate (int firstNum, int secondNum)
+            {
+                Console.WriteLine($"Sum is : {firstNum + secondNum}");
+            };
+            Action<int, int> subAction = delegate (int firstNum, int secondNum)
+            {
+                Console.WriteLine($"Difference is : {firstNum - secondNum}");
+            };
+            Action<int, int> mulAction = delegate (int firstNum, int secondNum)
+            {
+                Console.WriteLine($"Product is : {firstNum * secondNum}");
+            };
+            Action<int, int> divAction = delegate (int firstNum, int secondNum)
+            {
+                Console.WriteLine($"Reminder is : {firstNum / secondNum}");
+            };
+            addAction(150, 75);
+            subAction(75, 42);
+            mulAction(58, 4);
+            divAction(840, 42);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnonymousMethods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addAnonymous = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum + secondNum);
+            };
+
+            CalculateSum(addAnonymous);
+
+            Func<int, int, int> subAnonymous = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum - secondNum);
+            };
+
+            CalculateDifference(subAnonymous);
+
+            Func<int, int, int> mulAnonymous = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum * secondNum);
+            };
+
+            CalculateProduct(mulAnonymous);
+
+            Func<int, int, int> divAnonymous = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum / secondNum);
+            };
+
+            CalculateReminder(divAnonymous);
+        }
+        static void CalculateSum(Func<int, int, int> func)
+        {
+            Console.WriteLine($"Sum is : {func(170,34)}");
+
+        }
+        static void CalculateDifference(Func<int, int, int> func)
+        {
+            Console.WriteLine($"Difference is : {func(140, 72)}");
+
+        }
+        static void CalculateProduct(Func<int, int, int> func)
+        {
+            Console.WriteLine($"Product is : {func(16, 72)}");
+
+        }
+        static void CalculateReminder(Func<int, int, int> func)
+        {
+            Console.WriteLine($"Reminder is : {func(140, 42)}");
+
+        }
+    }
+}
